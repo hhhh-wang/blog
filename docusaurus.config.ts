57165312abce6cf55527a67a65e6f913c7c0d385
wangkun <kun64855@gmail.com>
2025-01-19 16:42:26 +0800

    调整了一下备案信息

---
+++ docusaurus.config.ts
@@ -0,0 +1,277 @@
+import type * as Preset from '@docusaurus/preset-classic'
+import type { Config } from '@docusaurus/types'
+import { themes } from 'prism-react-renderer'
+import social from './data/social'
+import type { GiscusConfig } from './src/components/Comment'
+
+const beian = '这是备案'
+const beian1 = '这也是备案'
+
+const config: Config = {
+  title: '变量人生',
+  url: 'https://127.0.0.1',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'bianliang',
+  projectName: 'blog',
+  customFields: {
+    bio: '无论前路如何，探索与学习是我不断进化的动力',
+    description:
+      '是一个由变量人生创建的个人博客，主要分享编程开发知识和项目，该网站基于 React 驱动的静态网站生成器 Docusaurus 构建。',
+  },
+  themeConfig: {
+    // announcementBar: {
+    //   id: 'announcementBar-3',
+    //   content: ``,
+    // },
+    image: 'img/index.png',
+    metadata: [
+      {
+        name: 'author',
+        content: '变量人生',
+      },
+      {
+        name: 'keywords',
+        content: 'blog, javascript, typescript, node, react, vue, web',
+      },
+      {
+        name: 'keywords',
+        content: '编程爱好者, Web开发者, TS全栈 ,部署运维',
+      },
+    ],
+    docs: {
+      sidebar: {
+        hideable: true,
+      },
+    },
+    navbar: {
+      logo: {
+        alt: '变量人生',
+        src: 'img/logo.webp',
+        srcDark: 'img/logo.webp',
+      },
+      hideOnScroll: true,
+      items: [
+        { label: '博客', position: 'right', to: 'blog' },
+        { label: '项目', position: 'right', to: 'project' },
+        { label: '友链', position: 'right', to: 'friends' },
+        { label: '关于', position: 'right', to: 'about' },
+        {
+          label: '更多',
+          position: 'right',
+          items: [
+            { label: '归档', to: 'blog/archive' },
+            { label: '笔记', to: 'docs/skill' },
+            { label: '工具推荐', to: 'docs/tools' },
+          ],
+        },
+        // {
+        //   type: 'localeDropdown',
+        //   position: 'right',
+        // },
+      ],
+    },
+    footer: {
+      style: 'light',
+      copyright: `
+        <div class="footer-wrapper">
+          <div class="footer-content">
+            <p class="beian-text">
+              <a href="http://beian.miit.gov.cn/" class="beian-link">${beian}</a>
+            </p>
+            <p class="police-beian">
+              <img src="/img/police.png" alt="police" class="police-icon"/>
+              <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=${
+                beian1.match(/\d+/)?.[0]
+              }" class="beian-link">${beian1}</a>
+            </p>
+            <p class="copyright-text">
+            <!-- 备案下方描述-->
+              <!--Copyright © 2020 - ${new Date().getFullYear()}. | Built with Docusaurus.-->
+              这是一个描述
+            </p>
+          </div>
+        </div>
+      `,
+    },
+    // todo 网站抓取配置暂时还没有，先不做
+    algolia: {
+      appId: 'GV6YN1ODMO',
+      apiKey: '50303937b0e4630bec4a20a14e3b7872',
+      indexName: 'kuizuo',
+    },
+    prism: {
+      theme: themes.oneLight,
+      darkTheme: themes.oneDark,
+      additionalLanguages: ['bash', 'json', 'java', 'python', 'php', 'graphql', 'rust', 'toml', 'protobuf', 'diff'],
+      defaultLanguage: 'javascript',
+      magicComments: [
+        {
+          className: 'theme-code-block-highlighted-line',
+          line: 'highlight-next-line',
+          block: { start: 'highlight-start', end: 'highlight-end' },
+        },
+        {
+          className: 'code-block-error-line',
+          line: 'This will error',
+        },
+      ],
+    },
+    giscus: {
+      repo: 'kuizuo/blog',
+      repoId: 'MDEwOlJlcG9zaXRvcnkzOTc2MjU2MTI=',
+      category: 'General',
+      categoryId: 'DIC_kwDOF7NJDM4CPK95',
+      theme: 'light',
+      darkTheme: 'dark_dimmed',
+    } satisfies Partial<GiscusConfig>,
+    tableOfContents: {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 4,
+    },
+    liveCodeBlock: { playgroundPosition: 'top' },
+    zoom: {
+      selector: '.markdown :not(em) > img',
+      background: {
+        light: 'rgb(255, 255, 255)',
+        dark: 'rgb(50, 50, 50)',
+      },
+    },
+  } satisfies Preset.ThemeConfig,
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          path: 'docs',
+          sidebarPath: 'sidebars.ts',
+        },
+        blog: false,
+        theme: {
+          customCss: ['./src/css/custom.css', './src/css/tweet-theme.css'],
+        },
+        sitemap: {
+          priority: 0.5,
+        },
+        gtag: {
+          trackingID: 'G-S4SD5NXWXF',
+          anonymizeIP: true,
+        },
+        debug: process.env.NODE_ENV === 'development',
+      } satisfies Preset.Options,
+    ],
+  ],
+  plugins: [
+    'docusaurus-plugin-image-zoom',
+    '@docusaurus/plugin-ideal-image',
+    // ['docusaurus-plugin-baidu-tongji', { token: 'c9a3849aa75f9c4a4e65f846cd1a5155' }],
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: process.env.NODE_ENV === 'development',
+        offlineModeActivationStrategies: ['appInstalled', 'standalone', 'queryString'],
+        pwaHead: [
+          { tagName: 'link', rel: 'icon', href: '/img/kuizuo.png' },
+          { tagName: 'link', rel: 'manifest', href: '/manifest.json' },
+          { tagName: 'meta', name: 'theme-color', content: '#12affa' },
+        ],
+      },
+    ],
+    [
+      'vercel-analytics',
+      {
+        debug: process.env.NODE_ENV === 'development',
+        mode: 'auto',
+      },
+    ],
+    [
+      './src/plugin/plugin-content-blog', // 为了实现全局 blog 数据，必须改写 plugin-content-blog 插件
+      {
+        path: 'blog',
+        editUrl: ({ locale, blogDirPath, blogPath, permalink }) =>
+          `https://github.com/kuizuo/blog/edit/main/${blogDirPath}/${blogPath}`,
+        editLocalizedFiles: false,
+        blogDescription: '代码人生：编织技术与生活的博客之旅',
+        blogSidebarCount: 10,
+        blogSidebarTitle: '博文',
+        postsPerPage: 12,
+        showReadingTime: true,
+        readingTime: ({ content, frontMatter, defaultReadingTime }) =>
+          defaultReadingTime({ content, options: { wordsPerMinute: 300 } }),
+        feedOptions: {
+          type: 'all',
+          title: '变量人生',
+          description: 'feedId:41215011978385457+userId:41840354283324416',
+          // <!-- 备案下方描述-->
+          // copyright: `Copyright © ${new Date().getFullYear()} 变量人生 Built with Docusaurus.<p><a href="http://beian.miit.gov.cn/" class="footer_lin">${beian}</a></p>`,
+           copyright: `这是描述<p><a href="http://beian.miit.gov.cn/" class="footer_lin">${beian}</a></p>`,
+        },
+      },
+    ],
+    async function tailwindcssPlugin() {
+      return {
+        name: 'docusaurus-tailwindcss',
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require('tailwindcss'))
+          postcssOptions.plugins.push(require('autoprefixer'))
+          return postcssOptions
+        },
+      }
+    },
+    async function injectMotto() {
+      return {
+        name: 'docusaurus-motto',
+        injectHtmlTags() {
+          return {
+            headTags: [
+              {
+                tagName: 'script',
+                innerHTML: `
+    (${function () {
+      console.log(
+        `%c Kz Blog %c https://github.com/kuizuo/blog`,
+        'color: #fff; margin: 1em 0; padding: 5px 0; background: #12affa;',
+        'margin: 1em 0; padding: 5px 0; background: #efefef;',
+      )
+
+      const motto = `
+This Webisite Powered By Kz Blog.
+Written by Docusaurus, Coding with Love.
+--------
+Love what you do and do what you love.
+`
+
+      if (document.firstChild?.nodeType !== Node.COMMENT_NODE) {
+        document.prepend(document.createComment(motto))
+      }
+    }.toString()})();`,
+              },
+            ],
+          }
+        },
+      }
+    },
+  ],
+  headTags: [
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'description',
+        content: '变量人生的个人博客',
+      },
+    },
+  ],
+  stylesheets: [
+    'https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Normal.min.css',
+    'https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Medium.min.css',
+    'https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Semibold.min.css',
+  ],
+  i18n: {
+    defaultLocale: 'zh-CN',
+    locales: ['zh-CN'],
+  },
+  onBrokenLinks: 'warn',
+}
+
+export default config
